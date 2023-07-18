52466d1e4f7396e4072ac6ffbf78f3bcce7fc835
Kadin Sayani <kadin.sayani@proton.me>
2023-07-18 14:40:31 -0600

    Added shorthand commands for log_level and log_format

--- src/main.rs
+++ src/main.rs
@@ -34,13 +34,13 @@ struct Cli {
     /// Log levels: error, warn, info, debug, trace
     ///
     /// Default is "info".
-    #[arg(short, long)]
+    #[arg(short = 'll', long)]
     log_level: Option<String>,
 
     /// Log formats: ecs, forest
     ///
     /// Default is "ecs".
-    #[arg(short, long)]
+    #[arg(short = 'lf', long)]
     log_format: Option<String>,
 
     #[command(subcommand)]
