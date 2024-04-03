409d33aff47026982aab4e35ff0d9e247fc5ab35
Bob Aman <bob@sporkmonger.com>
2024-04-02 18:51:34 -0700

    Put assertions back in

--- tests/envoy.rs
+++ tests/envoy.rs
@@ -42,11 +42,9 @@ async fn test_envoy_evil_bit() -> Result<(), Box<dyn std::error::Error>> {
 
     // send a friendly request to our envoy service
     let response = reqwest::get("http://127.0.0.1:8080").await?;
-    // assert!(response.status().is_success());
-    println!("response: {:?}", response);
+    assert!(response.status().is_success());
     let body = response.text().await?;
-    // assert!(body.contains("hello-world"));
-    println!("body: {:?}", body);
+    assert!(body.contains("hello-world"));
 
     // send an evil request to our envoy service
     let client = reqwest::Client::new();
@@ -55,11 +53,9 @@ async fn test_envoy_evil_bit() -> Result<(), Box<dyn std::error::Error>> {
         .header("Evil", "true")
         .send()
         .await?;
-    // assert!(response.status().is_client_error());
-    println!("response: {:?}", response);
+    assert!(response.status().is_client_error());
     let body = response.text().await?;
-    // assert!(body.contains("Access Denied"));
-    println!("body: {:?}", body);
+    assert!(body.contains("Access Denied"));
 
     Ok(())
 }
