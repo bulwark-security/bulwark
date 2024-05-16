525e353fd6a9126c5c5a706ba8d8d6e3aa4e9799
Bob Aman <bob@sporkmonger.com>
2024-05-16 15:55:26 -0700

    Strip commented out code

--- crates/ext-processor/src/protobuf.rs
+++ crates/ext-processor/src/protobuf.rs
@@ -6,9 +6,7 @@ pub mod envoy {
     pub mod config {
         pub mod core {
             pub mod v3 {
-                // tonic::include_proto!("xds");
                 include!(concat!(env!("OUT_DIR"), "/envoy.config.core.v3.rs"));
-                // include!(concat!(env!("OUT_DIR"), "/envoy.config.core.v3.serde.rs"));
             }
         }
     }
@@ -21,10 +19,6 @@ pub mod envoy {
                             env!("OUT_DIR"),
                             "/envoy.extensions.filters.http.ext_authz.v3.rs"
                         ));
-                        // include!(concat!(
-                        //     env!("OUT_DIR"),
-                        //     "/envoy.extensions.filters.http.ext_authz.v3.serde.rs"
-                        // ));
                     }
                 }
                 pub mod ext_proc {
@@ -33,10 +27,6 @@ pub mod envoy {
                             env!("OUT_DIR"),
                             "/envoy.extensions.filters.http.ext_proc.v3.rs"
                         ));
-                        // include!(concat!(
-                        //     env!("OUT_DIR"),
-                        //     "/envoy.extensions.filters.http.ext_proc.v3.serde.rs"
-                        // ));
                     }
                 }
             }
@@ -46,29 +36,21 @@ pub mod envoy {
         pub mod matcher {
             pub mod v3 {
                 include!(concat!(env!("OUT_DIR"), "/envoy.r#type.matcher.v3.rs"));
-                // include!(concat!(
-                //     env!("OUT_DIR"),
-                //     "/envoy.r#type.matcher.v3.serde.rs"
-                // ));
             }
         }
         pub mod v3 {
             include!(concat!(env!("OUT_DIR"), "/envoy.r#type.v3.rs"));
-            // include!(concat!(env!("OUT_DIR"), "/envoy.r#type.v3.serde.rs"));
         }
     }
     pub mod service {
         pub mod auth {
             pub mod v3 {
-                // tonic::include_proto!("google");
                 include!(concat!(env!("OUT_DIR"), "/envoy.service.auth.v3.rs"));
-                // include!(concat!(env!("OUT_DIR"), "/envoy.service.auth.v3.serde.rs"));
             }
         }
         pub mod ext_proc {
             pub mod v3 {
                 include!(concat!(env!("OUT_DIR"), "/envoy.service.ext_proc.v3.rs"));
-                // include!(concat!(env!("OUT_DIR"), "/envoy.service.ext_proc.v3.serde.rs"));
             }
         }
     }
@@ -76,24 +58,20 @@ pub mod envoy {
 pub mod google {
     pub mod rpc {
         include!(concat!(env!("OUT_DIR"), "/google.rpc.rs"));
-        // include!(concat!(env!("OUT_DIR"), "/google.rpc.serde.rs"));
     }
 }
 pub mod udpa {
     pub mod annotations {
         include!(concat!(env!("OUT_DIR"), "/udpa.annotations.rs"));
-        // include!(concat!(env!("OUT_DIR"), "/udpa.annotations.serde.rs"));
     }
 }
 pub mod validate {
     include!(concat!(env!("OUT_DIR"), "/validate.rs"));
-    // include!(concat!(env!("OUT_DIR"), "/validate.serde.rs"));
 }
 pub mod xds {
     pub mod core {
         pub mod v3 {
             include!(concat!(env!("OUT_DIR"), "/xds.core.v3.rs"));
-            // include!(concat!(env!("OUT_DIR"), "/xds.core.v3.serde.rs"));
         }
     }
 }
